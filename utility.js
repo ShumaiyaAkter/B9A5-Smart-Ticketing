ec0811f1d046a945c3774fcc1e27eaf3b4ceaebf
Shumaiya Akter <shumaiyaakter@9faf2f64-a3a2-45f6-a0b6-170f23589017.fritz.box>
2024-02-19 21:14:04 +0100

    working on button

--- utility.js
+++ utility.js
@@ -1,25 +1,37 @@
+
 function setSeatColor(elementId){
     const element=document.getElementById(elementId);
     element.classList.add('bg-green-500');
 
 }
+
 const seatButtons=document.querySelectorAll(".button")
 // video te cards=seatButtons
 console.log(seatButtons); 
+
+let count=0;
 
 for (let index=0;index<seatButtons.length;index++){
     const seat=seatButtons[index];
     console.log(seat);
      // video te card=seat
-    seat.addEventListener("click",function(){
+    seat.addEventListener("click",function(e){
         // console.log("clicked");
+        count=count+1;
+        setInnerText("seat-count",count);
+
 
         const seatNum=seat.querySelector("h3").innerText;
-        // const price=seat.querySelector("h4").innerText;
         console.log(seatNum)
         
 
      })
 
     
+ }
+
+
+//  innerText function
+ function setInnerText(id,value){
+    document.getElementById(id).innerText=value;
  }
