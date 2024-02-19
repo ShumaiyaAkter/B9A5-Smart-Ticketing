598f4b0eea29742a44d1bfc6ad128228a70f8159
Shumaiya Akter <shumaiyaakter@91cf1dce-7e53-410d-813d-221ef2347500.fritz.box>
2024-02-19 21:49:29 +0100

    seat selection upgraded

--- utility.js
+++ utility.js
@@ -18,11 +18,30 @@ for (let index=0;index<seatButtons.length;index++){
     seat.addEventListener("click",function(e){
         // console.log("clicked");
         count=count+1;
+        const seatNum=seat.querySelector("h3").innerText;
+        console.log(seatNum);
+
+        const selectedContainer=document.getElementById("selected-place-container");
+        const li=document.createElement("li");
+        const p=document.createElement("p");
+        const p2=document.createElement("p2");
+        const p3=document.createElement("p3");
+        
+
+        p.innerText=seatNum;
+        p2.innerText="Economy";
+        p3.innerText="550"
+
+        li.appendChild(p)
+        li.appendChild(p2)
+        li.appendChild(p3)
+        selectedContainer.appendChild(li);
+
+
         setInnerText("seat-count",count);
 
 
-        const seatNum=seat.querySelector("h3").innerText;
-        console.log(seatNum)
+        
         
 
      })
